05bf02d0b332576c6d8eb8648c281fe567803b3e
riku-0305 <rikuy03@icloud.com>
2025-06-30 00:29:16 +0900

    StudentCoursesクラスを作成して、フィールドとゲッター、セッターを作成。StudentRepositoryクラスに生徒のコース情報を全件取得するsearchCourses()メソッドを追加,Applicationクラスで呼び出すためにGetMappingでgetStudentCoursesメソッドを追加。

--- Student-Management/src/main/java/raisetech/Student/Management/Student.java
+++ Student-Management/src/main/java/raisetech/Student/Management/Student.java
@@ -1,23 +1,20 @@
 package raisetech.Student.Management;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
 public class Student {
 
+
+  private String id;
   private String name;
+  private String reading;
+  private String nickName;
+  private String mailAddress;
   private int age;
+  private String gender;
 
-  public String getName() {
-    return name;
-  }
-
-  public void setName(String name) {
-    this.name = name;
-  }
-
-  public int getAge() {
-    return age;
-  }
-
-  public void setAge(int age) {
-    this.age = age;
-  }
 }
